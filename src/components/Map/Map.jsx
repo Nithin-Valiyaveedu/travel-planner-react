8c46170e4ff1aeb2e7ec7206e30b674428137306
Nithin-Valiyaveedu <nithinvaliya@gmail.com>
2021-11-19 02:08:14 +0530

    dev google maps api implemented

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import { Rating } from "@material-ui/lab";
+
+import useStyles from "./styles";
 
 const Map = () => {
-    return (
-        
-            <h1>Map</h1>
-    )
-}
+  const classes = useStyles();
+  //set isMobile to false if the screensize is larger than 600px
+  const isMobile = useMediaQuery("(min-width:600px");
+  const coordinates = { lat: 10.784703, lng: 76.653145 };
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyCcYIqMZnUBjtRYYDIoAbTpvV-AdZH3VQI" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
+};
 
-export default Map
+export default Map;
